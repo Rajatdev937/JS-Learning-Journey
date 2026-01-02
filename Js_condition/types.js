b6b4fa3c944d19230013a7857f456b98951b4ba4
Rajat Negi <007negirajat@gmail.com>
2026-01-02 23:00:14 +0530

    New Problem

---
+++ Js_condition/types.js
@@ -0,0 +1,26 @@
+//In Jacascript we can execute a particular block of code using condition statement . 
+//if the condition meets the the block of code runs otherwise it doesnt. 
+
+//TYPES OF CONDITION STATEMENTS IN JS.
+
+//1.If-statement : Evaluate the condition  within  {} if the condition get true the code runs otherwise it skip.
+//eg- 
+let a=5;
+let b=6;
+if(a<b){
+    console.log("yes");
+}
+
+
+console.log("\n");
+//2.else-if Statment : Evlauate the condition within {} if the conditions get true other wise executes the execute the 
+// code within else part.
+//eg- 
+var c=19;
+if(c>=18){
+    console.log("You are an adult now");
+}
+else{
+    console.log("Not an Adult");
+}
+
