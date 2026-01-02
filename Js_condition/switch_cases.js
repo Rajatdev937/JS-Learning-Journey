e21aaa5d5238d79607be2bd44e55251bf3969d8e
Rajat Negi <007negirajat@gmail.com>
2026-01-02 23:52:28 +0530

    New Problem

---
+++ Js_condition/switch_cases.js
@@ -0,0 +1,34 @@
+/*SWITCH CASES : switch statement is a control structure in JavaScript that allows you to execute different blocks of code based on the value.if statements when you need to compare one variable against many possible values.*/
+
+//eg - 
+let day=3;
+
+switch(day){
+
+case 1: 
+console.log("Monday");
+break;
+
+case 2:
+console.log("Tuesday");
+break;
+
+case 3:
+console.log("Wednesday");
+break;
+
+case 4: 
+console.log("Thurday");
+break;
+
+case 5 :
+console.log("friday");
+
+case 6:
+console.log("Saturday");
+break;
+
+case 7:
+console.log("Sunday");
+break;
+}
