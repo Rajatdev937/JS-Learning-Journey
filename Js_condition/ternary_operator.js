a2744dee4c6651a674b36fe758511def9f979e15
Rajat Negi <007negirajat@gmail.com>
2026-01-03 13:12:19 +0530

    Update JS_condition.js

---
+++ Js_condition/ternary_operator.js
@@ -0,0 +1,12 @@
+//TERNARY OPERATOR :Evaluates a condition and returns one of two values based on whether the condition is true or false
+
+/* 
+syntax ~ 
+
+condition ? expressionIfTrue : expressionIfFalse ;
+
+*/
+let  age=15;
+var c=
+(age>=18)? "Eligible" : "Not Eligible";
+console.log(c);
