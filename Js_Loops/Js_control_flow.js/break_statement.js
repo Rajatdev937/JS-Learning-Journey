ff3960f1c6f4d062eb4553db13cab0b49ae4ba80
Rajat Negi <007negirajat@gmail.com>
2026-01-04 12:57:37 +0530

    updated files

---
+++ Js_Loops/Js_control_flow.js/break_statement.js
@@ -0,0 +1,14 @@
+//BEAK STATEMENT - Its is used to jump out of a iterating loop. 
+/* 
+syntax: 
+for(intiation,conditon,inc/dec){
+statement//
+if(condition)
+break;
+}
+*/
+for(let i=0;i<5;i++){
+if(i==3)
+    break;
+console.log(i);
+}
---
+++ Js_Loops/Js_control_flow.js/continue_statement.js
@@ -0,0 +1,8 @@
+//CONTINUE STATEMENT : The continue statement skips the current iteration of the loop and jumps to the  other .
+//eg- 
+
+for(let i=0;i<5;i++){
+    if(i==3)
+    continue;
+    console.log(i);
+}
