2c144bc1c9fb4ada8b51a40e178fb51dae9c8a4c
Rajat Negi <007negirajat@gmail.com>
2026-01-09 13:47:22 +0530

    Js.string

---
+++ Js_string/str_opr2.js
@@ -0,0 +1,25 @@
+//3.Concatenation : concatenation refers to the joininig of two different  strings. 
+
+let str1="Rajat";
+let str2="Negi";
+console.log(str1 +" "+ str2);
+
+//VII.white - space  trimmer: trim() cutoffs the unnecessary white space in a string.
+
+/* 
+syntax:
+str.trim()
+*/
+let str8="  ASUS ";
+console.log(str8.trim());//cut the unnecessary white spaces.
+
+
+//VIII.split() operation : split() breaks a string into an array based on a separator (delimiter).
+
+/* 
+syntax: 
+str.trim();
+*/
+let str9="Apple,banana,orange,guava,peach";
+console.log(str9.split(','));//seprate each string by ,
+console.log(str9.split(""));//seperate each character 
