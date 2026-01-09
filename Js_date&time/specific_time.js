6be0948a26e18e10c8e9f8907d7c77484e68963c
Rajat Negi <007negirajat@gmail.com>
2026-01-09 22:31:48 +0530

    date and time

---
+++ Js_date&time/specific_time.js
@@ -0,0 +1,10 @@
+//IN JS we can aslo get specific part of time 
+
+//ex-
+let now=new Date();
+console.log(now.getDate());//returns only date
+console.log(now.getDay());// "       "    day
+console.log(now.getMonth());// "      "   Month
+console.log(now.getHours());// "     "    hours
+console.log(now.getMinutes());// "   "    Minutes
+console.log(now.getFullYear());//"   "     year
