f90c31c58f548c09a78be2e923c6109187996885
Rajat Negi <007negirajat@gmail.com>
2026-01-10 23:14:12 +0530

    arrays

---
+++ Js_array/access_arr.js
@@ -0,0 +1,16 @@
+//In JS can access elements of an array using indexing.
+
+let arr=[1,3,4,5,"Rajat"];//[0,1,2,3,4,]
+console.log(arr[0]);
+console.log(arr[4]);
+
+//Mutability of Arrays:
+
+arr[4]='bikku';
+console.log(arr);
+
+//We can aslo find the length of an given array using -  arr.length function 
+
+//eg-
+let size=arr.length;
+console.log(size);
---
+++ Js_array/array.js
@@ -0,0 +1,14 @@
+//In JavaScript, an array is an ordered list of values. 
+// Each value, known as an element, is assigned a numeric position in the array called its index. 
+// The indexing starts at 0, so the first element is at position 0, the second at position 1, and so on.
+//Arrays can hold any type of data-such as numbers, strings, objects, or even other arrays—making them a flexible and essential part of JavaScript programming.
+
+
+//eg-
+let arr=[2,3,4,5];
+console.log(arr);
+
+//heterogenous array : An array which contains diff typ datatype elements.
+
+let array=[2,3.4,54,'a',true];
+console.log(array);
