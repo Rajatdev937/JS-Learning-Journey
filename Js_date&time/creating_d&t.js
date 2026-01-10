6f61b3c08f8252dc6f365cbd364efbf73dfdc047
Rajat Negi <007negirajat@gmail.com>
2026-01-10 14:03:11 +0530

    Updated date_obj.js

--- Js_date&time/creating_d&t.js
+++ Js_date&time/creating_d&t.js
@@ -2,3 +2,12 @@
 //eg-
 let now= new Date(2025,9,20,5,30,33,526);
 console.log(now);
+
+console.log("\n");
+
+const ms = Date.now();///returns date in milliseconds.
+console.log(ms);
+const dates= new Date(1768029178871);//converts milliseconds to normal date format
+console.log(dates);
+console.log(dates.toLocaleString());
+
