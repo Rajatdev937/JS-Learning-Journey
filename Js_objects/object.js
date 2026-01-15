8a3818eba28294507e72eaf7449d0dd4edd2f584
Rajat Negi <007negirajat@gmail.com>
2026-01-15 23:51:20 +0530

    Added JS objects file

---
+++ Js_objects/object.js
@@ -0,0 +1,10 @@
+//IN JS objects are the dynamic data structure that stores the related data in a key-value pain.
+//where each  key uniquely determines its value.
+
+let obj={
+    name:"rajat",
+    class:19,
+    sap_id:22841,
+    email_id:"007negirajat@gmail.com",
+};
+console.log(obj);
